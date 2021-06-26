57391104f36cffb08a1dc398b282a8de9b0e7136
petumazo <davidalvarezmazzo@gmail.com>
2021-06-25 23:16:39 -0500

    Setup root to main#index and and bootstrap to layout view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'main/index'
+  root 'main#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
